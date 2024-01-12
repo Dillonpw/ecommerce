5f204305547fb9a977401163b9505b67fa295422
dillonpw <dillonpwalsh10@gmail.com>
2024-01-12 01:20:15 -0500

    added github link and icon to footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,15 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+
 const Footer: React.FC = () => {
   return (
-    <footer className="mt-40 flex items-center justify-center bg-gray-400 p-20 text-center text-sm opacity-60">
+    <footer className="mt-40 flex items-center justify-center bg-slate-800 text-white p-20 text-center text-sm ">
       <p>
-        Created by Dillon Walsh <br /> &copy;2024 All Rights Reserved
+        Created by Dillonpw{" "}
+        <a target="blank_" href="https://github.com/Dillonpw">
+          <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <br /> &copy;2024 All Rights Reserved
       </p>
     </footer>
   );
